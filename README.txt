12d408fc1370523c151bd4c5e642ebdd061e0799
Charles He <charles@hbtagency.com.au>
2015-11-20 14:52:26 +1100

    Front-end framework

---
+++ README.txt
@@ -0,0 +1,21 @@
+Readme:
+
+1.Install Node.js
+2.Run "npm install -g gulp"
+
+(Or copy the whole node_modules file)
+Gulp-watch
+- Run "npm install --save-dev gulp-watch"
+Gulp-concat-css
+- Run "npm install --save-dev gulp-concat-css"
+Gulp-minify-css
+- Run "npm install --save-dev gulp-minify-css"
+Gulp-sass 
+- Run "npm install --save-dev gulp-sass"
+Gulp-batch
+- Run "npm install --save-dev gulp-sass"
+Browser-sync
+- Run "npm install --save-dev browser-sync"
+
+3.Setup file path in gulpfile.js
+3.Run "gulp"
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+var concatCss = require('gulp-concat-css');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var browserSync = require('browser-sync');
+
+
+gulp.task('default', function() {
+
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+  
+  watch('css/project/*.scss', batch(function (events, done) {
+        gulp.start('sass', done);
+    }));
+  watch('css/cache/sassOutput/*.css', batch(function (events, done) {
+        gulp.start('concat', done);
+    }));
+  watch('css/cache/conOutput/*.css', batch(function (events, done) {
+        gulp.start('minify', done);
+
+    }));
+});
+
+//Step 1: compile sass
+gulp.task('sass', function () {
+  gulp.src('css/project/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/cache/sassOutput'));
+    console.log('#=>sass');
+});
+
+//Step 2: concat CSS files
+gulp.task('concat', function () {
+  console.log('#=>concat');
+  return gulp.src('css/cache/sassOutput/*.css')
+  // return gulp.src(['css/cache/sassOutput/*.css','css/bootstrap/*.css'])
+    .pipe(concatCss("style.css"))
+    .pipe(gulp.dest('css/cache/conOutput/'));
+});
+
+//Step 3: minify CSS file and reload browser
+gulp.task('minify', function() {
+  console.log('#=>minify');
+  return gulp.src('css/cache/conOutput/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css/final/'))
+    .pipe(browserSync.reload({stream:true}));
+});
